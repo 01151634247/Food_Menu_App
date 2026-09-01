e0ccf12f174a5f3da2b748eab81471fbb9b56366
ayman <aymanmohamed1142001@gmail.com>
2026-09-01 21:23:44 +0300

    create model

--- lib/Domain/custom_text_field.dart
+++ lib/Domain/custom_text_field.dart
@@ -3,59 +3,45 @@ import 'package:flutter/material.dart';
 
    class Text{
 
-  static TextStyle textStyle15 =TextStyle(
+   static TextStyle textStyle12 =TextStyle(
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+    fontFamily: 'primary',
+    fontSize: 12,
+  ); 
+
+
+   static TextStyle textStyle18 =TextStyle(
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+    fontFamily: 'primary',
+    fontSize: 18,
+  ); 
+
+  static TextStyle textStyle24 =TextStyle(
     fontWeight: FontWeight.normal,
     color: Colors.black,
     fontFamily: 'primary',
     fontSize: 15,
   );
 
-  static TextStyle textStyle18 =TextStyle(
+
+   static TextStyle textStyle36 =TextStyle(
     fontWeight: FontWeight.normal,
     color: Colors.black,
     fontFamily: 'primary',
-    fontSize: 18,
+    fontSize: 36,
   );
 
-
-  static TextStyle textStyle20 =TextStyle(
+  static TextStyle textStyle48 =TextStyle(
     fontWeight: FontWeight.normal,
     color: Colors.black,
     fontFamily: 'primary',
-    fontSize: 20,
-  );
-
- static  TextStyle textStyle22 =TextStyle(
-    fontWeight: FontWeight.normal,
-    color: Colors.black,
-    fontFamily: 'primary',
-    fontSize: 22,
+    fontSize: 48,
   );
 
 
- static TextStyle textStyle25 =TextStyle(
-    fontWeight: FontWeight.normal,
-    color: Colors.black,
-    fontFamily: 'primary',
-    fontSize: 25,
-  );
-
-
-static TextStyle textStyle28 =TextStyle(
-    fontWeight: FontWeight.normal,
-    color: Colors.black,
-    fontFamily: 'primary',
-    fontSize: 28,
-  );
-
-  static TextStyle textStyle30 =TextStyle(
-    fontWeight: FontWeight.normal,
-    color: Colors.black,
-    fontFamily: 'primary',
-    fontSize: 30,
-
-
-  );
+  
 
 
 
